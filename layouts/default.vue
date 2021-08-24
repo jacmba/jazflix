13631cfe41e546da944e2b4600efbfae601c8aa0
Jacinto Mba <jcantero@fexco.com>
2021-08-24 08:45:51 +0100

    Added Jazflix logo to navigation drawer

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,6 +8,9 @@
       app
     >
       <v-list>
+        <v-list-item>
+          <v-img src="https://fontmeme.com/permalink/210824/53a131e0396614b0fd10a53a4c44ce42.png"></v-img>
+        </v-list-item>
         <v-list-item
           v-for="(item, i) in items"
           :key="i"
