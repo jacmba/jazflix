a28548446ad98047b76848731b93d27869c45104
Jacinto Mba Cantero <jacmba@gmail.com>
2022-02-18 19:52:41 +0000

    Up to 4 grid columns on larger screens

--- components/MovieGrid.vue
+++ components/MovieGrid.vue
@@ -23,6 +23,8 @@
         cols="12"
         sm="8"
         md="6"
+        lg="4"
+        xl="3"
         align="center"
         justify="center"
       >
--- test/JazflixLogo.spec.js
+++ test/JazflixLogo.spec.js
@@ -1,9 +1,24 @@
-import { mount } from '@vue/test-utils'
+import { createLocalVue, mount } from '@vue/test-utils'
 import JazflixLogo from '@/components/JazflixLogo.vue'
+import Vuetify from 'vuetify'
 
 describe('JazflixLogo', () => {
+  const localVue = createLocalVue()
+  let vuetify
+
+  beforeEach(() => {
+    vuetify = new Vuetify()
+  })
+
   test('is a Vue instance', () => {
-    const wrapper = mount(JazflixLogo)
+    const wrapper = mount(JazflixLogo, { localVue, vuetify })
     expect(wrapper.vm).toBeTruthy()
+
+    const link = wrapper.find('.nuxt-link')
+    expect(link).toBeDefined()
+    expect(link.attributes.to).toBe('/')
+
+    const img = link.find('.v-img')
+    expect(img).toBeDefined()
   })
 })
